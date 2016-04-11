0abf47250b451bd971d98ca03a689773e8d9e005
Jong Wook Kim <diamon153@gmail.com>
2016-04-11 13:21:55 +0900

    First Commit

---
+++ app/src/main/java/com/example/jongwook/trial/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.jongwook.trial;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private CaldroidFragment caldroidFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar1, caldroidFragment);
+        t.commit();
+    }
+
+    public void changeToCal(View view) {
+        Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+        startActivity(intent);
+        return;
+    }
+}
